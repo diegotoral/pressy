6f4570d3a2b037113d6ad1ae40d9c0d42ed7e1e6
Diego Toral <diegotoral@gmail.com>
2019-11-17 14:56:01 -0300

    Add Rails UJS and update landing layout
    We're using Rails UJS as a simple and quick way to post with delete
    method, etc.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,9 +1,12 @@
 import 'bulma'
 import 'trix'
 import '@rails/actiontext'
+
+import UJS from '@rails/ujs'
 import Turbolinks from 'turbolinks'
 import Notifications from '../common/notifications'
 
+UJS.start()
 Turbolinks.start()
 
 document.addEventListener('turbolinks:load', () => {
