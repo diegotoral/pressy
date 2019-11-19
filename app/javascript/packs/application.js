bf930686e6730d0641b413459f6a2f79491e4017
Diego Toral <diegotoral@gmail.com>
2019-11-19 01:24:38 -0300

    Configure Vue and Turbolinks
    Also added a small snippet to load props from data attribute so we can
    easily pass data to Vue components.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,12 +6,35 @@ import '@rails/actiontext'
 import '../stylesheets/actiontext.scss'
 
 import UJS from '@rails/ujs'
+import Vue from 'vue/dist/vue.esm'
 import Turbolinks from 'turbolinks'
+import TurbolinksAdapter from 'vue-turbolinks'
 import Notifications from '../common/notifications'
+
+import App from '../app.vue'
 
 UJS.start()
 Turbolinks.start()
 
+Vue.use(TurbolinksAdapter)
+
 document.addEventListener('turbolinks:load', () => {
+  const element = document.getElementById("app")
+  const props = JSON.parse(element.getAttribute('data'))
+
   Notifications.start()
+
+  if (element != null && props != null) {
+    const app = new Vue({
+      el: '#app',
+
+      data: () => {
+        return {
+          message: "Can you say hello?"
+        }
+      },
+
+      render: h => h(App, { props }),
+    })
+  }
 })
